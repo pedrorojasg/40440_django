6759f5df3fbae5dc484cecf25c8c3b7a20aea9f8
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-04-21 09:56:04 -0400

    Agregadas primeras views

--- sistema_coder/urls.py
+++ sistema_coder/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from sistema_coder.views import saludar, saludar_con_fecha, saludar_a_usuario
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("saludo/", saludar),
+    path("saludo-hoy/", saludar_con_fecha),
+    path("hello/<nombre>/", saludar_a_usuario),
 ]
---
+++ sistema_coder/views.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def saludar(request):
+    saludo = "Hola querido usuario"
+    pagina_html = HttpResponse(saludo)
+    return pagina_html
+
+
+def saludar_con_fecha(request):
+    hoy = datetime.now()
+    saludo = f"Hola querido usuario, fecha: {hoy.day}/{hoy.month}"
+    pagina_html = HttpResponse(saludo)
+    return pagina_html
+
+
+def saludar_a_usuario(request, nombre):
+    texto = f"Hola {nombre}"
+    pagina_html = HttpResponse(texto)
+    return pagina_html
