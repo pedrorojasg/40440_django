d8dc3984c6099bf2def62215326c0c07781fcf70
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-05-02 19:55:43 -0400

    Agregados str y blank True al modelo Profesor

--- control_estudios/models.py
+++ control_estudios/models.py
@@ -25,10 +25,13 @@ class Profesor(models.Model):
     apellido = models.CharField(max_length=256)
     nombre = models.CharField(max_length=256)
     dni = models.CharField(max_length=32)
-    email = models.EmailField()
+    email = models.EmailField(blank=True)
     fecha_nacimiento = models.DateField()
     profesion = models.CharField(max_length=128)
-    bio = models.TextField()
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
 
 
 class Entregable(models.Model):
