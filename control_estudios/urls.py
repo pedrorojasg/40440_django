6bf1552bd04958decc298f49f3d73c54cec6f8a0
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-05-09 19:41:08 -0400

    Agregadas vistas de clases para Estudiantes

--- control_estudios/urls.py
+++ control_estudios/urls.py
@@ -17,15 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from control_estudios.views import listar_estudiantes, listar_cursos, crear_curso,\
-    buscar_cursos, eliminar_curso, editar_curso
+from control_estudios.views import listar_cursos, crear_curso, buscar_cursos,\
+    eliminar_curso, editar_curso, EstudianteListView, EstudianteCreateView,\
+    EstudianteDetailView, EstudianteUpdateView, EstudianteDeleteView
 
 
 urlpatterns = [
-    path("estudiantes/", listar_estudiantes, name="lista_estudiantes"),
+    # URLS de cursos
     path("cursos/", listar_cursos, name="lista_cursos"),
     path("crear-curso/", crear_curso, name="crear_curso"),
     path("buscar-cursos/", buscar_cursos, name="buscar_cursos"),
-    path('eliminar-curso/<int:id>/', eliminar_curso, name="eliminar_curso"),
-    path('editar-curso/<int:id>/', editar_curso, name="editar_curso"),
+    path("eliminar-curso/<int:id>/", eliminar_curso, name="eliminar_curso"),
+    path("editar-curso/<int:id>/", editar_curso, name="editar_curso"),
+    # URLS de estudiantes
+    path("estudiantes/", EstudianteListView.as_view(), name="lista_estudiantes"),
+    path('estudiantes/<int:pk>/', EstudianteDetailView.as_view(), name="ver_estudiante"),
+    path('crear-estudiante/', EstudianteCreateView.as_view(), name="crear_estudiante"),
+    path('editar-estudiante/<int:pk>/', EstudianteUpdateView.as_view(), name="editar_estudiante"),
+    path('eliminar-estudiante/<int:pk>/', EstudianteDeleteView.as_view(), name="eliminar_estudiante"),
 ]
--- control_estudios/views.py
+++ control_estudios/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from control_estudios.forms import CursoFormulario
 from control_estudios.models import Estudiante, Curso
 
 
-def listar_estudiantes(request):
-    contexto = {
-        "estudiantes": Estudiante.objects.all(),
-    }
-    http_response = render(
-        request=request,
-        template_name='control_estudios/lista_estudiantes.html',
-        context=contexto,
-    )
-    return http_response
+# NO USADO
+# def listar_estudiantes(request):
+#     contexto = {
+#         "estudiantes": Estudiante.objects.all(),
+#     }
+#     http_response = render(
+#         request=request,
+#         template_name='control_estudios/lista_estudiantes.html',
+#         context=contexto,
+#     )
+#     return http_response
 
-
+# Vistas de cursos
 def listar_cursos(request):
     contexto = {
         "cursos": Curso.objects.all(),
@@ -121,3 +123,31 @@ def editar_curso(request, id):
         template_name='control_estudios/formulario_curso.html',
         context={'formulario': formulario},
     )
+
+
+# Vistas de Estudiantes
+class EstudianteListView(ListView):
+    model = Estudiante
+    template_name = 'control_estudios/lista_estudiantes.html'
+
+
+class EstudianteCreateView(CreateView):
+    model = Estudiante
+    fields = ('apellido', 'nombre', 'email', 'dni')
+    success_url = reverse_lazy('lista_estudiantes')
+
+
+class EstudianteDetailView(DetailView):
+    model = Estudiante
+    success_url = reverse_lazy('lista_estudiantes')
+
+
+class EstudianteUpdateView(UpdateView):
+    model = Estudiante
+    fields = ('apellido', 'nombre', 'email', 'dni')
+    success_url = reverse_lazy('lista_estudiantes')
+
+
+class EstudianteDeleteView(DeleteView):
+    model = Estudiante
+    success_url = reverse_lazy('lista_estudiantes')
