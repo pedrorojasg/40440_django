25bf41572241e24df70af866bccaa947d5fbdc98
Pedro Rojas Gavidia <pedro.rojas.g@example.com>
2023-05-16 22:29:04 -0400

    Agregado pruebas unitarias

--- control_estudios/tests.py
+++ control_estudios/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.db import IntegrityError
+from control_estudios.models import Curso
+
+
+class CursoTests(TestCase):
+    """En esta clase van todas las pruebas del modelo Curso."""
+
+    def test_creacion_curso(self):
+        # caso uso esperado
+        curso = Curso(nombre="Titulo", comision=1000)
+        curso.save()
+
+        # Compruebo que el curso fue creado y la data fue guardad correctamente
+        self.assertEqual(Curso.objects.count(), 1)
+        self.assertEqual(curso.nombre, "Titulo")
+        self.assertEqual(curso.comision, 1000)
+
+    def test_curso_str(self):
+        curso = Curso(nombre="Python", comision=20000)
+        curso.save()
+
+        # Compruebo el str funciona como se desea
+        self.assertEqual(curso.__str__(), "Python | 20000")
